2ffeb378afd813d72b27a52730786a731b2f3a6c
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-04-13 09:03:23 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+"name": "الخزينة",
+"summary": "Manage Khazina.",
+"author": "kaddour Abdelaziz",
+"category": "Services/Khazina",
+"license": "AGPL-3",
+"website": "https://github.com/PacktPublishing"
+"/Odoo-15-Development-Essentials",
+"version": "15.0.1.0.0",
+"depends": ["base"],
+"application": True,
+"data": [
+"security/khazina_security.xml",
+"security/ir.model.access.csv",
+"views/khazina_view.xml",
+"views/khazina_menu.xml",
+],
+}
---
+++ models/khazina_book.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+
+class Book(models.Model):
+ _name = "khazina.book"
+ _description = "Khazina Book"
+ name = fields.Char("Title", required=True)
+ isbn = fields.Char("ISBN")
+ active = fields.Boolean("Active?", default=True)
+ date_published = fields.Date()
+ image = fields.Binary("Cover")
+ publisher_id = fields.Many2one("res.partner",string="Publisher")
+ author_ids = fields.Many2many("res.partner",string="Authors")
+
+
+
+
+ def button_check_isbn(self):
+    print("Hi  Hi HI")
