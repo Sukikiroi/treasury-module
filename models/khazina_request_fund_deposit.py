1609f1088d6243683f383c437a9b005adb53068a
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-04-21 10:04:16 +0100

    update: add new veiw

--- models/khazina_request_fund_deposit.py
+++ models/khazina_request_fund_deposit.py
@@ -15,21 +15,26 @@ class RequestFundDeposit(models.Model):
     date = fields.Date(string='بتاريخ اليوم',default=date.today().strftime('%Y-%m-%d'))
     time = fields.Char(string='ساعة اليوم',default=datetime.now().strftime("%H:%M:%S"))
     recodnumber =fields.Integer(string= 'رقم المحضر',compute='_get_record_id')
-    recodreference =fields.Char(string= 'رقم المرجع')
-    record_editor_name =fields.Many2one('res.users','Current User', default=lambda self: self.env.user)
+    recodreference =fields.Char(string= 'رقم المرجع',compute='_get_reference_number')
+    record_editor_name =fields.Many2one('res.users',' اسم و لقب المحرر', default=lambda self: self.env.user)
     
     record_editor_position =fields.Char(string= 'منصب محرر المحضر')
-    amount_number=fields.Integer(string= 'المبلغ المعاين باالحروف')
+    amount_number=fields.Integer(string= 'المبلغ المعاين با الأرقام')
     amount_letters=fields.Char(string= 'المبلغ المعاين بالحروف')
     signature = fields.Binary(string=' ختم و توقيع المسؤول التجاري')
     secretary_treasury_signature= fields.Binary(string=' ختم و توقيع أمين الخزينة')
-    state=fields.Selection([('request','طلب'),('order','أمر'),('testimonial','اشهاد')])
+    state=fields.Selection([('request','طلب'),('order','أمر'),('done','تم التنفيد')])
     note = fields.Html('Notes')
 
 
 
     def _get_record_id(self):
         self.recodnumber=self.env['khazina.request_fund_deposit'].search([])[-1].id
+        
+
+    def _get_reference_number(self):
+        self.recodreference=str(self.env['khazina.request_fund_deposit'].search([])[-1].id)+"123"
+
 
     def do_clear_done():
         print('yeh yeh')    
